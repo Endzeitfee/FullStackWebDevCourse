bcd156f8ffcb785e1bbecbe05382bfd20f1b90c4
Endzeitfee <52233973+Endzeitfee@users.noreply.github.com>
2020-04-13 15:01:29 +0200

    Add files via upload

---
+++ addingJStoHTML-day5/addtoHTML.js
@@ -0,0 +1,13 @@
+function buttonAlert() {
+    alert("You clicked Me!");
+    alert("Practice some clicking... NOW!");
+    alert("Well done! 9998 more clicks to becoming a master!");
+}
+
+function buttonDate() {
+    alert(Date());
+}
+
+function pChange() {
+    document.getElementById("changeText").innerHTML="new TEXT!";
+}
