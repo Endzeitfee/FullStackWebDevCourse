4fa59f3e54a4f5214c40d3693816d107c64c1d35
Endzeitfee <52233973+Endzeitfee@users.noreply.github.com>
2020-04-23 16:59:31 +0200

    Add files via upload

---
+++ addingJStoHTML-day6/addtoHTML.js
@@ -0,0 +1,47 @@
+function buttonAlert() {
+    alert("You clicked Me!");
+    alert("Practice some clicking... NOW!");
+    alert("Well done! 9998 more clicks to becoming a master!");
+}
+
+function buttonDate() {
+    alert(Date());
+}
+
+function pChange() {
+    document.getElementById("changeText").innerHTML="new TEXT!";
+}
+
+function changeCss() {
+    document.getElementById("boringP").innerHTML="This is the new fun paragraph?!";
+    document.getElementById("boringP").style.backgroundColor="slategrey";
+    document.getElementById("boringP").style.border="3px solid linen";
+    document.body.style.backgroundColor="violet";
+}
+
+function changeDivOne() {
+    document.getElementById("divOne").style.backgroundColor="magenta";
+}
+
+function changeDivTwo() {
+    document.getElementById("divTwo").style.backgroundColor="cyan";
+    document.body.style.backgroundColor="purple";
+}
+
+function changeText() {
+    document.getElementById("para").innerHTML=":O ...you have changed all the text!"
+}
+
+
+function backBlue() {
+    document.body.style.backgroundColor="darkblue";
+}
+
+function doChange() {
+    document.body.style.backgroundColor="violet";
+}
+
+function backRed() {
+    document.body.style.backgroundColor="orangered";
+}
+
